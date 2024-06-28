443ddb10fce063eb2b8929a9783557fe536c6e40
Jan Truong <162881848+maithtruong@users.noreply.github.com>
2024-06-28 21:53:29 +0700

    Add solutions for Module 1 Week 4 exercises

---
+++ MODULE_1/WEEK_4/multiple_choice.py
@@ -0,0 +1,37 @@
+from levenshtein_distance import levenshtein_distance
+import streamlit as st
+
+# Q1 - A
+print("Q1 Answer: A")
+
+# Q2 - C
+print("Q2 Answer: C")
+
+options = st.multiselect('Your favourite colors:', ['Green', 'Yellow', 'Red', 'Blue'], ['Yellow', 'Red'])
+print('You selected:', options)
+
+# Q3 - D
+print("Q3 Answer: D")
+
+# Q4 - C
+print("Q4 Answer: C")
+
+# Q5 - A
+print("Q5 Answer: A")
+
+print(levenshtein_distance('elmets', 'elements'))
+
+# Q6 - D
+print("Q6 Answer: D")
+
+# Q7 - D
+print("Q7 Answer: D")
+
+# Q8 - B
+print("Q8 Answer: B")
+
+# Q9 - D
+print("Q9 Answer: D")
+
+# Q10 - B
+print("Q10 Answer: B")
